9a6342ddde6994fa59208751cc5caddca2b70829
Felipe <felipe@localhost.localdomain>
2023-06-24 20:39:38 -0300

    Added formatted property to the User model

--- src/User/index.js
+++ src/User/index.js
@@ -1,5 +1,20 @@
 const Model = require('../_Model')
 
-const User = (databasePassword) => Model('User', databasePassword)
+const User = (databasePassword) => ({
+  ...Model('User', databasePassword),
+  formatted (user) {
+    return {
+      selectedAddress: user.selectedAddress,
+      name: user.name,
+      surname: user.surname,
+      birthday: user.birthday,
+      cpf: user.cpf,
+      email: user.email,
+      paymentMethods: user.paymentMethods,
+      favorites: user.favorites,
+      orders: user.orders
+    }
+  }
+})
 
 module.exports = User
