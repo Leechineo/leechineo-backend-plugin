5b68f8aae001f708a80eb3420578806b9a7bbdac
Felipe <felipe@localhost.localdomain>
2023-06-24 22:22:21 -0300

    Added formatted model function to Brand model

--- src/Brand/index.js
+++ src/Brand/index.js
@@ -1,5 +1,17 @@
 const Model = require('../_Model')
 
-const Brand = (databasePassword) => Model('Brand', databasePassword)
+const Brand = (databasePassword) => ({
+  ...Model('Brand', databasePassword),
+  formatted (brand) {
+    return {
+      id: brand.id,
+      name: brand.name,
+      icon: brand.icon,
+      color: brand.color,
+      brandWebsite: brand.brandWebsite,
+      createdAt: brand.createdAt
+    }
+  }
+})
 
 module.exports = Brand
