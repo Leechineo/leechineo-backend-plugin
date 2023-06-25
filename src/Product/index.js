5f0bbd7a5e451edfd4e88e332cc9f9d18cb8efad
Felipe <felipe@localhost.localdomain>
2023-06-25 15:11:46 -0300

    Added product formatted schema

--- src/Product/index.js
+++ src/Product/index.js
@@ -3,8 +3,22 @@ const findFullProduct = require('./findFullProduct')
 const findFullProducts = require('./findFullProducts')
 const findAndFilter = require('./findAndFilter')
 
+const productFomarttedSchema = (product) => ({
+  id: product.id,
+  name: product.name,
+  description: product.description,
+  images: product.images,
+  specification: product.specification,
+  category: product.category,
+  brand: product.brand,
+  type: product.type,
+  stocks: product.stocks,
+  private: product.private,
+  createdAt: product.createdAt
+})
+
 const Product = (databasePassword) => ({
-  ...Model('Product', databasePassword),
+  ...Model('Product', databasePassword, productFomarttedSchema),
   findFullProduct: findFullProduct(databasePassword),
   findFullProducts: findFullProducts(databasePassword),
   findAndFilter: findAndFilter(databasePassword)
