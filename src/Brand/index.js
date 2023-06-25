62ade4887a1e07200cad485b661b972113ed526a
Felipe <felipe@localhost.localdomain>
2023-06-25 01:10:27 -0300

    Moved formatted function to _Model

--- src/Brand/index.js
+++ src/Brand/index.js
@@ -1,17 +1,14 @@
 const Model = require('../_Model')
 
-const Brand = (databasePassword) => ({
-  ...Model('Brand', databasePassword),
-  formatted (brand) {
-    return {
-      id: brand.id,
-      name: brand.name,
-      icon: brand.icon,
-      color: brand.color,
-      brandWebsite: brand.brandWebsite,
-      createdAt: brand.createdAt
-    }
-  }
+const formattedBrandSchema = (brand) => ({
+  id: brand.id,
+  name: brand.name,
+  icon: brand.icon,
+  color: brand.color,
+  brandWebsite: brand.brandWebsite,
+  createdAt: brand.createdAt
 })
+
+const Brand = (databasePassword) => Model('Brand', databasePassword, formattedBrandSchema)
 
 module.exports = Brand
--- src/User/index.js
+++ src/User/index.js
@@ -1,21 +1,17 @@
 const Model = require('../_Model')
 
-const User = (databasePassword) => ({
-  ...Model('User', databasePassword),
-  formatted (user) {
-    return {
-      selectedAddress: user.selectedAddress,
-      name: user.name,
-      surname: user.surname,
-      birthday: user.birthday,
-      cpf: user.cpf,
-      email: user.email,
-      paymentMethods: user.paymentMethods,
-      favorites: user.favorites,
-      orders: user.orders,
-      createdAt: user.createdAt
-    }
-  }
+const formattedUserSchema = (user) => ({
+  selectedAddress: user.selectedAddress,
+  name: user.name,
+  surname: user.surname,
+  birthday: user.birthday,
+  cpf: user.cpf,
+  email: user.email,
+  paymentMethods: user.paymentMethods,
+  favorites: user.favorites,
+  orders: user.orders,
+  createdAt: user.createdAt
 })
+const User = (databasePassword) => Model('User', databasePassword, formattedUserSchema)
 
 module.exports = User
