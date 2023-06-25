c5a6923c06ce5e84c3404221ec571f5e7265502c
Felipe <felipe@localhost.localdomain>
2023-06-25 01:51:32 -0300

    formattedCategorySchema added

--- src/Category/index.js
+++ src/Category/index.js
@@ -1,9 +1,16 @@
 const Model = require('../_Model')
 const findCategoryById = require('./findCategoryById')
 
+const formattedCategorySchema = (category) => ({
+  id: category.id,
+  name: category.name,
+  subcategories: category.subcategories,
+  createdAt: category.createdAt
+})
+
 const Category = (databasePassword) => ({
-  ...Model('Category', databasePassword),
-  findCategoryById: findCategoryById()
+  ...Model('Category', databasePassword, formattedCategorySchema),
+  findCategoryById: findCategoryById(databasePassword)
 })
 
 module.exports = Category
